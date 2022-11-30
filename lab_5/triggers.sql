8fe4c2d7c666a5e43f35a51a1f3d0268be80abbf
Aleh <Trapper.MD@mail.ru>
2022-11-30 12:52:54 +0300

    Update triggers.sql

--- lab_5/triggers.sql
+++ lab_5/triggers.sql
@@ -15,3 +15,46 @@ CREATE OR REPLACE FUNCTION inc_winc_count() RETURNS TRIGGER AS $wins_count$
 
 CREATE TRIGGER inc_wins_count AFTER UPDATE OR INSERT ON grand_prix
 	FOR EACH ROW EXECUTE PROCEDURE inc_winc_count();
+
+
+
+CREATE OR REPLACE FUNCTION wins_logging() RETURNS TRIGGER AS $wins_logging$
+	BEGIN
+	IF OLD.winner_id IS NULL AND NEW.winner_id IS NOT NULL THEN
+		INSERT INTO logs
+		(date_of_log,time_of_log, log_message)
+		VALUES
+		(CAST(NOW() AS DATE), cast(NOW() AS TIME), 
+		 (SELECT first_name || ' ' || second_name 
+			FROM pilots
+		   	WHERE pilot_id = NEW.winner_id) || ' wins ' || NEW.grand_prix_name);
+		RETURN NEW;
+	END IF;
+	RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+END;
+$wins_logging$ LANGUAGE plpgsql;
+
+CREATE TRIGGER wins_logging
+AFTER INSERT OR UPDATE ON grand_prix
+	FOR EACH ROW EXECUTE PROCEDURE wins_logging();
+
+
+CREATE OR REPLACE FUNCTION qualis_logging() RETURNS TRIGGER AS $qualis_logging$
+	BEGIN
+	IF OLD.quali_winner_id IS NULL AND NEW.quali_winner_id IS NOT NULL THEN
+		INSERT INTO logs
+		(date_of_log,time_of_log, log_message)
+		VALUES
+		(CAST(NOW() AS DATE), cast(NOW() AS TIME), 
+		 (SELECT first_name || ' ' || second_name 
+			FROM pilots
+		   	WHERE pilot_id = NEW.quali_winner_id) || ' wins ' || NEW.grand_prix_name || ' qualification');
+		RETURN NEW;
+	END IF;
+	RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+END;
+$qualis_logging$ LANGUAGE plpgsql;
+
+CREATE TRIGGER qualis_logging
+AFTER INSERT OR UPDATE ON grand_prix
+	FOR EACH ROW EXECUTE PROCEDURE qualis_logging();
